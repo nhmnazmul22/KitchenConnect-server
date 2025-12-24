e3add48ab3fdb6e4fe6c6c9d1b42e05f40fc72e0
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-24 23:30:23 +0600

    added cors origin

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(async (req, res, next) => {
   console.log(
