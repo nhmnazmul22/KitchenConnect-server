0ddfdc072e0ccec7e41a79ee7ecbaee6a5028dd1
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-28 10:55:57 +0600

    fix: update SameSite cookie attribute for production environment

--- http/services/tokenServices.js
+++ http/services/tokenServices.js
@@ -32,7 +32,7 @@ export const generateToken = async (req, res, collection) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "lax" : "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
