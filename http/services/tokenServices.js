e70962e8351a747471f4e76452156709f1b7b835
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-28 11:04:28 +0600

    fix: set secure cookie attribute to true for all environments

--- http/services/tokenServices.js
+++ http/services/tokenServices.js
@@ -31,7 +31,7 @@ export const generateToken = async (req, res, collection) => {
   const token = TokenEncoded(payload);
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
