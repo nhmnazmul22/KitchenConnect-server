219798f0fa7d18d7734eddcc898b43d258c36855
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-28 11:08:02 +0600

    fix: update cookie attributes for secure and SameSite handling based on environment

--- http/services/tokenServices.js
+++ http/services/tokenServices.js
@@ -31,8 +31,8 @@ export const generateToken = async (req, res, collection) => {
   const token = TokenEncoded(payload);
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
